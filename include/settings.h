244b8f804c7726f942875eb1c154ab43d5fadad2
Paolo Patierno <ppatierno@live.com>
2019-08-28 15:58:10 +0200

    First application development for sending to Kafka via HTTP bridge

---
+++ include/settings.h
@@ -0,0 +1,7 @@
+const char *ssid = "<WIFI-SSID>";
+const char *password = "<WIFI-PASSWORD>";
+
+const char *bridge = "<HTTP-BRIDGE-URL>";
+const char *topic = "<KAFKA-TOPIC>";
+
+const char *deviceId = "<DEVICE-ID>";
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,74 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+#include "settings.h"
+
+char endpoint[100];
+char payload[100];
 
 void setup() {
-  // put your setup code here, to run once:
+  delay(1000);
+  
+  Serial.begin(9600);                  
+  Serial.println("ESP8266 HTTP based sensors device");
+
+  WiFi.mode(WIFI_OFF);
+  delay(1000);
+  WiFi.mode(WIFI_STA);
+
+  WiFi.begin(ssid, password);
+  Serial.println("");
+ 
+  Serial.print("Connecting");
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // initialize LED digital pin as an output.
+  pinMode(2, OUTPUT);
+
+  sprintf(endpoint, "http://%s/topics/%s", bridge, topic);
+  Serial.print("Bridge endpoint: ");
+  Serial.println(endpoint);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(2, HIGH);
+  delay(1000);
+
+  // turn the LED off by making the voltage LOW
+  digitalWrite(2, LOW);
+
+  // wait for a second
+  delay(1000);
+
+  WiFiClient client;
+  HTTPClient http;
+  
+  // establish connection with the bridge
+  http.begin(client, endpoint);
+  http.addHeader("Content-Type", "application/vnd.kafka.json.v2+json");
+
+  // build the payload
+  int temperature = 29;
+  sprintf(payload, "{ \"records\": [ { \"key\": \"%s\", \"value\": { \"deviceId\": \"%s\", \"temperature\": %d } } ] }", 
+          deviceId, deviceId, temperature);
+  Serial.print("Sending...");
+  Serial.println(payload);
+
+  // send the payload and close connection
+  http.POST(payload);
+  http.end();
+
+  delay(5000);
 }
